45a0ac91ca7e09e2acc0913a357f60658ec4ef13
Emile Belanger <emile.belanger@gmail.com>
2019-02-09 11:32:25 +0000

    Fix audio WakeLock

--- src/sound/oalsound.cpp
+++ src/sound/oalsound.cpp
@@ -1957,6 +1957,10 @@ void OpenALSoundRenderer::SetSfxPaused(bool paused, int slot)
 	}
 }
 
+#ifdef __ANDROID__
+extern "C" void OpenSL_android_set_pause( ALCdevice_struct *Device, int pause );
+#endif
+
 void OpenALSoundRenderer::SetInactive(SoundRenderer::EInactiveState state)
 {
 	switch(state)
@@ -1968,6 +1972,9 @@ void OpenALSoundRenderer::SetInactive(SoundRenderer::EInactiveState state)
 				alcDeviceResumeSOFT(Device);
 				getALCError(Device);
 			}
+#ifdef __ANDROID__
+            OpenSL_android_set_pause( Device, 0 );
+#endif
 			break;
 
 		case SoundRenderer::INACTIVE_Complete:
@@ -1979,6 +1986,9 @@ void OpenALSoundRenderer::SetInactive(SoundRenderer::EInactiveState state)
 			/* fall-through */
 		case SoundRenderer::INACTIVE_Mute:
 			alListenerf(AL_GAIN, 0.0f);
+#ifdef __ANDROID__
+            OpenSL_android_set_pause( Device, 1 );
+#endif
 			break;
 	}
 }
